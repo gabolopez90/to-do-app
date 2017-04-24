ab35734bf4a2569470b749177ad5387a8a78c11e
Gabriel Lopez <gabolopez1990@gmail.com>
2017-04-24 18:41:24 +0000

    app terminada, faltan retoques esteticos

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,18 +1,40 @@
-var data = [{item: "get milk"}, {item: "walk dog"}, {item:"buy food"}, {item: "train"}];
 var bodyParser = require("body-parser");
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
+var mongoose = require("mongoose");
+
+//Conectar a la base de datos
+mongoose.connect("mongodb://gabo:user@ds117271.mlab.com:17271/to-do");
+
+//Crear un schema - Una especia de plano
+var todoSchema = new mongoose.Schema({
+    item: String
+});
+
+var Todo = mongoose.model("Todo", todoSchema);
+
+
 module.exports = function(app){
     
     app.get("/todo", function(req,res){
-        res.render("todo", {todos: data});
+        Todo.find({}, function(err, data){
+            if (err) throw err;
+            res.render("todo", {todos: data});
+        });
     });
     
     app.post("/todo", urlencodedParser, function(req,res){
-        data.push(req.body);
-        res.json(data);
+        var newTodo = Todo(req.body).save(function(err, data){
+            if (err) throw err;
+            res.render("todo", {todos: data});
+        });
+        
+        
     });
     
-    app.delete("/todo",function(req,res){
-        
+    app.delete("/todo/:item",function(req,res){
+        Todo.find({item : req.params.item.replace(/\-/g," ")}).remove(function(err, data){
+            if(err) throw err;
+            res.render("todo", {todos: data});
+        })
     });
 };
